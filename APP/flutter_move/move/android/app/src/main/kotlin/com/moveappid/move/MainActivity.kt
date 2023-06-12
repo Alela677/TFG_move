e72ee83fc2b7fc7d6de9bbea17d550349a12a60e
Alela677 <93326492+Alela677@users.noreply.github.com>
2023-06-12 22:07:39 +0200

    Delete APP/flutter_move directory

--- APP/flutter_move/move/android/app/src/main/kotlin/com/moveappid/move/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.moveappid.move
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- APP/flutter_move/move/lib/global_utils/edades.dart
+++
@@ -1,6 +0,0 @@
-List<int> llenarListaEdad(List<int> edades) {
-  for (var i = 16; i < 100; i++) {
-    edades.add(i);
-  }
-  return edades;
-}
